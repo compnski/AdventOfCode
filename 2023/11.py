46266cff13018aeaa084411b67990b58e0aaafd4
Jason Freidman <jason@modelbit.com>
2023-12-11 01:24:36 -0500

    11 part 2!

--- 2023/11.py
+++ 2023/11.py
@@ -15,24 +15,38 @@ data = open("11.txt").read().strip()
 rows = np.array([[*s] for s in data.split("\n")])
 print(rows.shape)
 
-event_rows = []
-event_cols = []
-for idx in range(rows.shape[0] - 1, -1, -1):
+offset_x = []
+offset_y = []
+row_offset = 0
+col_offset = 0
+OFFSET_AMOUNT = 999999
+
+for idx in range(0, rows.shape[0]):
     has_galaxy = np.sum(rows[idx] == "#") > 0
     if not has_galaxy:
-        event_rows.append(idx)
-        rows = np.insert(rows, idx + 1, ["."] * rows.shape[1], axis=0)
-        print("insert!", rows.shape[1], ["."] * rows.shape[1])
+        row_offset += OFFSET_AMOUNT
+    offset_x.append(idx + row_offset)
 
-for idx in range(rows.shape[1] - 1, -1, -1):
+for idx in range(0, rows.shape[1]):
     has_galaxy = np.sum(rows[:, idx] == "#") > 0
     if not has_galaxy:
-        event_cols.append(idx)
-        rows = np.insert(rows, idx + 1, ["."] * rows.shape[0], axis=1)
-        print("insert!", rows.shape[0], ["."] * rows.shape[0])
+        col_offset += OFFSET_AMOUNT
+    offset_y.append(idx + col_offset)
+print(row_offset, col_offset)
 
-print(rows.shape)
-print(np.array2string(rows, separator="", formatter={"all": lambda x: x}))
+print(offset_x, offset_y)
+
+#[(0, 4), (1, 9), (2, 0), (5, 8), (6, 1), (7, 12), (10, 9), (11, 0), (11, 5)]
+
+
+def fix_space_time(x, y):
+    return (offset_x[x], offset_y[y])
+
+
+galaxies = [fix_space_time(x, y) for x, y in zip(*np.where(rows == "#"))]
+print(galaxies)
+#print(rows.shape)
+#print(np.array2string(rows, separator="", formatter={"all": lambda x: x}))
 
 
 def manhattan(a, b):
@@ -41,11 +55,11 @@ def manhattan(a, b):
 
 
 sum_dist = 0
-galaxies = [*zip(*np.where(rows == "#"))]
+
 for idx, galaxy in enumerate(galaxies):
     for other_galaxy in galaxies[idx + 1:]:
         dist = manhattan(galaxy, other_galaxy)
-        print(galaxy, other_galaxy, dist)
+        #print(galaxy, other_galaxy, dist)
         sum_dist += dist
 
 print(f"sum_dist = {sum_dist}")
